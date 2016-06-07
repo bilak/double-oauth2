1de273edc2ad2fca080d323c8b1bf056f439cd4b
bilak <bilaak@gmail.com>
2016-06-07 13:41:33 +0200

    - reconfigured oauth2 clients

--- authserver/src/main/java/demo/AuthserverApplication.java
+++ authserver/src/main/java/demo/AuthserverApplication.java
@@ -93,8 +93,7 @@ public class AuthserverApplication extends WebMvcConfigurerAdapter {
 
     @Configuration
     @EnableAuthorizationServer
-    protected static class OAuth2AuthorizationConfig extends
-            AuthorizationServerConfigurerAdapter {
+    protected static class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
 
         @Autowired
         private AuthenticationManager authenticationManager;
@@ -119,8 +118,10 @@ public class AuthserverApplication extends WebMvcConfigurerAdapter {
             clients.inMemory()
                     .withClient("acme")
                     .secret("acmesecret")
-                    .authorizedGrantTypes("authorization_code", "refresh_token",
-                            "password").scopes("openid");
+                    .authorizedGrantTypes("authorization_code", "refresh_token", "password")
+                    //.autoApprove(".*") // uncomment this to disable approval page
+                    .scopes("openid");
+                    //.redirectUris("http://localhost:8080");
         }
 
         @Override
@@ -130,14 +131,15 @@ public class AuthserverApplication extends WebMvcConfigurerAdapter {
                     .authorizationCodeServices(authorizationCodeServices())
                     .accessTokenConverter(accessTokenConverter())
                     .authenticationManager(authenticationManager)
-                    .tokenStore(tokenStore());
+                    .tokenStore(tokenStore())
+                    .approvalStoreDisabled();
         }
 
         @Override
-        public void configure(AuthorizationServerSecurityConfigurer oauthServer)
-                throws Exception {
-            oauthServer.tokenKeyAccess("permitAll()").checkTokenAccess(
-                    "isAuthenticated()");
+        public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+            oauthServer
+                    .tokenKeyAccess("permitAll()")
+                    .checkTokenAccess("isAuthenticated()");
         }
 
     }
